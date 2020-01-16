b69eb5aac5f6384f40da16160cec945f97b1366b
Dannie Schumaker <draeschumaker@gmail.com>
2020-01-15 21:32:04 -0800

    write getRandomThrow function

---
+++ app.js
@@ -0,0 +1,6 @@
+const userChoice = document.getElementById('user-choice');
+const computerChoice = document.getElementById('computer-choice');
+const totalWinsSpan = document.getElementById('user-wins');
+const totalLossesSpan = document.getElementById('computer-wins');
+const totalDrawsSpan = document.getElementById('draws');
+const submitButton = document.getElementById('submit-button');
---
+++ get-random-throw.js
@@ -0,0 +1,11 @@
+export default function getRandomThrow() {
+    let randomNumber = (Math.floor(Math.random() * 3));
+
+    if (randomNumber === 0) {
+        return 'rock';
+    } else if (randomNumber === 1) {
+        return 'paper';
+    } else if (randomNumber === 2) {
+        return 'scissors';
+    }
+}
